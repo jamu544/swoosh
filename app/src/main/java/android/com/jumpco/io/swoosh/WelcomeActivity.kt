ac49d544868a240b8ebe90045d1c31c41ff0ae2c
odwa <odwa.menziwa@jumpco.io>
2022-02-10 15:45:18 +0200

    first commit

---
+++ app/src/main/java/android/com/jumpco/io/swoosh/WelcomeActivity.kt
@@ -0,0 +1,11 @@
+package android.com.jumpco.io.swoosh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
